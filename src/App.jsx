20bb6efe2b55641c9087c28ea1b2f0e8d260c912
Gabriel Ferreira <gabriielbs@gmail.com>
2023-02-06 22:03:21 -0300

    feat: request setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Header} from "./components/header";
 import {Highlights} from "./components/highlights/highlights";
 import {Navbar} from "./components/navbar";
+import {fetcher} from "./services";
 import {Grid, GridItem} from "./ui/grid";
 
 function App() {
+  useEffect(() => {
+    const makeRequest = async () => {
+      const response = await fetcher("photos");
+      console.log(response);
+    };
+
+    makeRequest();
+  }, []);
   return (
     <Grid>
       <GridItem>
