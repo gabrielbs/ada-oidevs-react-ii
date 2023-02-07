43eb70dc51a0ea7929f4fbe0638edf9ea6c420d4
Gabriel Ferreira <gabriielbs@gmail.com>
2023-02-06 21:34:49 -0300

    feat: funcional example

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {Header} from "./components/header";
 import {Highlights} from "./components/highlights/highlights";
 import {Navbar} from "./components/navbar";
--- src/components/highlights/highlights.jsx
+++ src/components/highlights/highlights.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {Avatar} from "../avatar";
 import * as S from "./styles";
 
@@ -22,9 +23,38 @@ const highlightList = [
     image: "https://via.placeholder.com/90",
     text: "Modelo Educacional",
   },
+  {
+    image: "https://via.placeholder.com/90",
+    text: "",
+  },
+  {
+    image: "https://via.placeholder.com/90",
+    text: undefined,
+  },
 ];
 
+const extractTextStrings = (item) => {
+  if (!item.text) return "NONE";
+  return item.text;
+};
+
+export const removeInvalidValues = (item) => {
+  if (item === "NONE") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
 export const Highlights = () => {
+  const normalizeData = (data) =>
+    data.map(extractTextStrings).filter(removeInvalidValues);
+
+  React.useEffect(() => {
+    const meuArray = normalizeData(highlightList);
+    // console.log(meuArray);
+  }, []);
+
   return (
     <S.Wrapper>
       {highlightList.map((highlight, index) => (
